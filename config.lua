8435ec699a68774a35d6792710ed549ddefbd5e3
TheCicus <158371402+TheCicus@users.noreply.github.com>
2024-06-24 13:40:47 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -18,6 +18,7 @@ CICUS_KeyPad.Main = {
                     --  rot = vector3(0.000, 0.000, -89.716), -- Rotazione del keypad
                     -- door_model = 1557126584, modello della porta
                     -- jobs = {{job = "police", job_grade = 3}, {job = "ambulance", job_grade = 1}}  -- Jobs che possono accedere con grado
+                    -- double_door_model = "" -- Se ce la secoda porta metti il modello
                 --},
 
                 ["keypad_05"] = {
@@ -25,19 +26,20 @@ CICUS_KeyPad.Main = {
                     rot = vector3(0.000, 0.000, 31.283),
                     door_model = -131296141,
                     duoble_door = true,
+                    double_door_model = -131296141,
                     jobs = {{job = "police"}, {job = "ambulance", job_grade = 3}}
                 },
             }
         }
     },
 
-    Custom_Notify = function () 
+    Custom_Notify = function () -- Notifica di quando sei vicino. Se non usi ox_target
         AddTextEntry("CICUS_KeyPad_ALERT", "Premi ~INPUT_CONTEXT~ per aprire la porta")
         BeginTextCommandDisplayHelp("CICUS_KeyPad_ALERT")
         EndTextCommandDisplayHelp(0, true, true, -1)
     end,
 
-    Using_ox_target = false,
+    Using_ox_target = false, -- OX_target ?
 }
 
 
